595f95786fd20608be826e565685c8c2a8373ca7
t-l-xin <e0556433@u.nus.edu>
2021-08-22 16:08:01 +0800

    Add Echo method, Remove Greet.java
    Echo the user commands.
    Greet only has behaviours, does not need a specific class.
    
    Duke.java contains Greet, Echo, Exit methods.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,55 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static String logo =
+            " ______________                                   \n" +
+            "|            @ | #####  ####        #####   ####  \n" +
+            "|         @@   |   #   #    #       #    # #    # \n" +
+            "|       @@     |   #   #    # ##### #    # #    # \n" +
+            "|  @@  @@      |   #   #    #       #    # #    # \n" +
+            "|   @@@        |   #   #    #       #    # #    # \n" +
+            "|    @         |   #    ####        #####   ####  \n" +
+            " --------------";
+
+    public static String separator = "\n" + "_".repeat(60) + "\n";
+
+    public static void greetUser(){
+        String helloGreeting = "Hi! I'm LingLing! " +
+                "\nYour personal nanny to make sure you do work. \n"
+                + "Have you started ur tasks? Start by typing \"help\"";
+        System.out.println(logo + separator + helloGreeting + separator);
+    }
+
+    public static void echoCommand(){
+        String inputLine;
+        Scanner in = new Scanner(System.in);
+        boolean isBye = false;
+        do{
+            System.out.print("Type your command: \n");
+            inputLine = in.nextLine();
+            isBye = checkBye(inputLine);
+            if(!isBye){
+                System.out.println(separator + "echo " + inputLine + separator);
+            }
+        }while (!isBye);
+        exitProgram();
+    }
+
+    public static boolean checkBye(String command){
+        String byeString = "bye";
+        if(command.equals(byeString)){
+            return true;
+        }
+        return false;
+    }
+
+    public static void exitProgram(){
+        String exitGreeting = "Well done for today! Remember to have 8 hrs of sleep!";
+        System.out.println(separator + exitGreeting + separator);
+    }
+
     public static void main(String[] args) {
-        Greet.greetUserAndExit();
+        greetUser();
+        echoCommand();
     }
 }
--- src/main/java/Greet.java
+++
@@ -1,8 +0,0 @@
-public class Greet {
-    public static void greetUserAndExit(){
-        String line = "____________________________________________________________\n";
-        String helloGreeting = " Hello! I'm Duke\n What can I do for you?\n";
-        String exitGreeting = " Bye. Hope to see you again soon!\n";
-        System.out.println(line + helloGreeting + line + exitGreeting + line);
-    }
-}
